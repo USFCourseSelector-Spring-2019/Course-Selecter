09c2f3219d4208ac8a36c1351e68df646b916254
Nick Perez <nperez0111@gmail.com>
2018-04-22 15:16:09 -0700

    input into server only if not required

--- server/index.js
+++ server/index.js
@@ -1,30 +1,17 @@
-const scraper = require('../usf/index'),
+const scraper = require('../usf/Courses/index'),
     Promise = require('bluebird'),
     mydb = require('nano')('http://localhost:5984')
-
-Promise.promisifyAll(mydb);
-const getAndUse = dbName => Promise.promisifyAll(mydb.use(dbName))
-scraper.loadFile().then(data => {
-    getAndUse('usf').insertAsync({ ...data }).then((body) => {
-        console.log(body)
-        console.log('Successfully put all USF Data!')
-    }).catch((err) => {
-        console.log(err)
-    })
-
-    /*Promise.all(Object.entries(data.criteria).map(([key, docs]) => {
-        return (new Promise((resolve, reject) => {
-            mydb.db.destroy(key, function (err) {
-                resolve()
-            })
-        })).then(() => new Promise(resolve => mydb.db.create(key, () => resolve()))).then(() => {
-            return getAndUse(key).bulkAsync({ docs:docs.map(val=>({value:val})) }).then(body => {
-                console.log(body)
-            })
+    
+if (!module.parent) {
+    Promise.promisifyAll(mydb);
+    const getAndUse = dbName => Promise.promisifyAll(mydb.use(dbName))
+    scraper().then(data => {
+        data._id = data.accessDate
+        getAndUse('usf').insertAsync({ ...data }).then((body) => {
+            console.log(body)
+            console.log('Successfully put all USF Data!')
+        }).catch((err) => {
+            console.log(err)
         })
-
-    })).then(() => {
-        console.log('Successfully put criteria')
-    }).catch(err => { console.log(err) })*/
-
-})
+    })
+}
