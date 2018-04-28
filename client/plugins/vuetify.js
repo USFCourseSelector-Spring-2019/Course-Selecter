b31607d13dd264fcf06bb2e90e679cc62b59f4b0
Nick Perez <nperez0111@gmail.com>
2018-04-27 19:02:08 -0700

    moved components to own object

--- client/plugins/vuetify.js
+++ client/plugins/vuetify.js
@@ -23,60 +23,20 @@ import {
     VTooltip
 } from 'vuetify'
 export default (context, inject) => {
-    if(process.server){
-    const db = new PouchDB( /*context.isDev*/ true ? 'http://localhost:5984/usf' : 'http://db.courseselector.com/usf')
-    return db.get('theme').then((theme) => {
-        delete theme._rev
-        delete theme._id
+    if (process.server) {
+        const db = new PouchDB( /*context.isDev*/ true ? 'http://localhost:5984/usf' : 'http://db.courseselector.com/usf')
+        return db.get('theme').then((theme) => {
+            delete theme._rev
+            delete theme._id
+            Vue.use(Vuetify, {
+                components,
+                theme
+            })
+        })
+    } else {
         Vue.use(Vuetify, {
-            components: {
-                VApp,
-                VCard,
-                VNavigationDrawer,
-                VFooter,
-                VList,
-                VBtn,
-                VIcon,
-                VGrid,
-                VToolbar,
-                VExpansionPanel,
-                VTextField,
-                VSelect,
-                VAlert,
-                VDataTable,
-                VTabs,
-                VDialog,
-                VSubheader,
-                VBottomSheet,
-                VTooltip
-            },
-            theme
+            components
         })
-    })
-}else{
-    Vue.use(Vuetify, {
-            components: {
-                VApp,
-                VCard,
-                VNavigationDrawer,
-                VFooter,
-                VList,
-                VBtn,
-                VIcon,
-                VGrid,
-                VToolbar,
-                VExpansionPanel,
-                VTextField,
-                VSelect,
-                VAlert,
-                VDataTable,
-                VTabs,
-                VDialog,
-                VSubheader,
-                VBottomSheet,
-                VTooltip
-            }
-        })
-}
+    }
 
 }
