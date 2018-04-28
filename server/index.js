19abf553f8650dad6be3d3b35f3816c69671a917
Nick Perez <nperez0111@gmail.com>
2018-04-27 20:56:48 -0700

    made server properly update revision for couch

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
 const scraper = require('../usf/Courses/index'),
     Promise = require('bluebird'),
     mydb = require('nano')('http://localhost:5984')
-    
+
 if (!module.parent) {
     Promise.promisifyAll(mydb);
     const getAndUse = dbName => Promise.promisifyAll(mydb.use(dbName))
     scraper().then(data => {
         data._id = 'courses'
-        getAndUse('usf').insertAsync({ ...data }).then((body) => {
-            console.log(body)
-            console.log('Successfully put all USF Data!')
-        }).catch((err) => {
-            console.log(err)
+        const usf = getAndUse('usf')
+        usf.getAsync('courses').then(doc => {
+            data._rev = doc._rev
+            usf.insertAsync({ ...data }).then((body) => {
+                console.log(body)
+                console.log('Successfully put all USF Data!')
+            }).catch((err) => {
+                console.log(err)
+            })
         })
     })
 }
