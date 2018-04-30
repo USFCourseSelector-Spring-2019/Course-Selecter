96752833197ba8ebe15479d2c85d66192930592a
Nick Perez <nperez0111@gmail.com>
2018-04-29 22:43:25 -0700

    got some planner stuff going

--- client/components/Planner.vue
+++ client/components/Planner.vue
@@ -58,53 +58,16 @@
                     </v-toolbar-items>
                 </v-toolbar>
                 <v-card-text>
-                    You can add, edit, delete plans, you can change display options
-                    <v-list three-line subheader>
-                        <v-subheader>Add Plan</v-subheader>
-                        <v-list-tile avatar>
-                            <v-list-tile-content>
-                                <v-list-tile-title>Content filtering</v-list-tile-title>
-                                <v-list-tile-sub-title>Set the content filtering level to restrict apps that can be downloaded</v-list-tile-sub-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                        <v-list-tile avatar>
-                            <v-list-tile-content>
-                                <v-list-tile-title>Password</v-list-tile-title>
-                                <v-list-tile-sub-title>Require password for purchase or use password to restrict purchase</v-list-tile-sub-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </v-list>
-                    <v-divider></v-divider>
-                    <v-list three-line subheader>
-                        <v-subheader>General</v-subheader>
-                        <v-list-tile avatar>
-                            <v-list-tile-action>
-                                <v-checkbox v-model="notifications"></v-checkbox>
-                            </v-list-tile-action>
-                            <v-list-tile-content>
-                                <v-list-tile-title>Notifications</v-list-tile-title>
-                                <v-list-tile-sub-title>Notify me about updates to apps or games that I downloaded</v-list-tile-sub-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                        <v-list-tile avatar>
-                            <v-list-tile-action>
-                                <v-checkbox v-model="sound"></v-checkbox>
-                            </v-list-tile-action>
-                            <v-list-tile-content>
-                                <v-list-tile-title>Sound</v-list-tile-title>
-                                <v-list-tile-sub-title>Auto-update apps at any time. Data charges may apply</v-list-tile-sub-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                        <v-list-tile avatar>
-                            <v-list-tile-action>
-                                <v-checkbox v-model="widgets"></v-checkbox>
-                            </v-list-tile-action>
-                            <v-list-tile-content>
-                                <v-list-tile-title>Auto-add widgets</v-list-tile-title>
-                                <v-list-tile-sub-title>Automatically add home screen widgets</v-list-tile-sub-title>
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </v-list>
+                    <v-layout column>
+                        <v-layout v-for="(curPlan,i) in plans" :key="i">
+                            <v-text-field v-model="curPlan.title" :label="`Plan #${i+1} Title`"></v-text-field>
+                            <v-btn @click.native.stop="planner.plan=i" :disabled="planner.plan===i">{{planner.plan===i?'Is Current Plan':'Set as Current Plan'}}</v-btn>
+                        </v-layout>
+                    </v-layout>
+                    <v-btn @click.native.stop="plans.push({title: `Plan #${plans.length+1}`,courses: []})" color="primary">
+                        <v-icon left>add</v-icon>
+                        Add A Plan
+                    </v-btn>
                 </v-card-text>
                 <div style="flex: 1 1 auto;"></div>
             </v-card>
