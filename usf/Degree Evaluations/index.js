cf155a13f5b3c8e97dd82cda287eb663747a7996
Nick Perez <nperez0111@gmail.com>
2018-09-08 18:39:25 -0700

    parsing degree-eval pdf

--- usf/Degree Evaluations/index.js
+++ usf/Degree Evaluations/index.js
@@ -1,141 +1,71 @@
 const cheerio = require('cheerio'),
     write = require('write-json-file'),
     fs = require('fs'),
-    getHTML = require('./puppet'),
-    path = require('path')
-let obj = {}
-String.prototype.trimSplitLines = function () {
-    return this.trim().split("\n").map(str => str.trim())
-}
-String.prototype.trimLines = function () {
-    return this.trimSplitLines().join("")
+    path = require('path'),
+    execa = require('execa'),
+    obj = {},
+    FILE_NAME = './degree-eval.pdf'
+
+function rmbr(line) {
+    return line.replace('<br>', '')
 }
 
 function loadFile(file) {
-    return new Promise((resolve, reject) => fs.readFile(file, function (err, html) {
-        if (err) {
-            return reject(err)
-        }
-        resolve(html.toString())
-    }))
+    return Promise.resolve(execa.stdout('pdftohtml', ['-stdout', '-q', file]))
 }
 
 function scraper(doTheThing) {
     console.log("Transforming data to JSON...")
-    return doTheThing().then(html => cheerio.load(html)).then($ => {
-        const [accessedBy, semester, accessDate] = $('.staticheaders').text().trimSplitLines(),
-            headers = $('table.datadisplaytable').find('tr')[1].children.map(node => $(node).text().trimLines()).filter(a => a.length),
-            length = headers.length,
-            criteria = {
-                attributes: [],
-                instructors: [],
-                categories: [],
-                titles: [],
-                subjects: {}
-            }
-
-        function transformVal(val, key, index, row, headers) {
-            const transformer = {
-                Crse: val => {
-
-                    const subject = row[headers.indexOf('Subj')]
-                    if (subject.length) {
-                        if (!criteria.subjects[subject]) {
-                            criteria.subjects[subject] = []
-                        }
-                        if (!criteria.subjects[subject].includes(val) && val.length) {
-                            criteria.subjects[subject].push(val)
-                        }
-                    }
-                    return val
-                },
-                Select: (val) => {
-                    return val !== 'C'
-                },
-                Title: (val) => {
-                    if (!criteria.titles.includes(val)) {
-                        if (val && val.length) {
-                            criteria.titles.push(val)
-                        }
-                    }
-                    return val
-                },
-                Instructor: (val) => {
-                    val=val.split(" ").filter(a=>a.length).join(" ")
-                    if (!criteria.instructors.includes(val)) {
-                        if (val && val !== "TBA" && val.length) {
-                            criteria.instructors.push(val)
-                        }
-                    }
-                    return val
-                },
-                Attribute: (val) => {
-                    val.split(" and ").forEach(attr => {
-                        if (!criteria.attributes.includes(attr)) {
-                            if (attr && attr.length) {
-                                criteria.attributes.push(attr)
-                            }
-                        }
-                    })
-                    return val
-                },
-                Days: (val) => {
-                    const arr = [];
-                    ["M", "T", "W", "R", "F", "S", "U"].forEach((cur, i) => {
-                        if (val.includes(cur)) {
-                            arr.push(cur)
-                        }
-                    })
-                    return arr
-                },
-                Time: val => val.split("-"),
-                'Date (MM/DD)': val => val.split("-")
-            }
-            if (transformer[key]) {
-                return transformer[key](val, index)
-            }
-            return val
-        }
-
-
-        const courses = Array.from($('table.datadisplaytable').find('tr')).slice(0).reduce((arr, cur, i) => {
-            if (i > -1) {
-                const row = Array.from($(cur).find('td')).map(node => $(node).text().trimLines())
-                const header = Array.from($(cur).find('th'))
-                if (header.length == 1) {
-                    criteria.categories.push($(header[0]).text())
+    return doTheThing().then(text => {
+        const searchFor = {
+            'Student<br>': (line, i, lines) => {
+                const [lastname, firstname, middlename] = rmbr(lines[i + 1]).split(" ")
+                obj.firstname = firstname
+                obj.middlename = middlename
+                obj.lastname = lastname.slice(0, -1)
+            },
+            'Class<br>': (line, i, lines) => {
+                obj.level = rmbr(lines[i + 1])
+            },
+            'Major<br>': (line, i, lines) => {
+                obj.major = rmbr(lines[i + 1])
+            },
+            'Minor<br>': (line, i, lines) => {
+                if (lines[i + 1] !== 'Academic Standing<br>') {
+                    obj.minor = rmbr(lines[i + 1])
                 }
-                if (row.length) {
-                    const obj = row.reduce((obj, val, i, row) => {
-                        const key = headers[i]
-                        obj[key] = transformVal(val, key, i, row, headers)
-                        return obj
-                    }, {})
-                    obj.Category = criteria.categories[criteria.categories.length - 1]
-                    return arr.concat(obj)
+            },
+            'prerequisite<br>': (line, i, lines) => {
+                if (lines[i + 1] === 'Catalog Year:<br>') {
+                    const required = parseInt(rmbr(lines[i + 4]))
+                    const applied = parseInt(rmbr(lines[i + 7]))
+                    obj.total_credits_required = required
+                    obj.total_credits_applied = applied
                 }
-            }
-            return arr
-        }, [])
-
-
-
-        obj = {
-            semester,
-            accessDate: new Date(accessDate),
-            criteria,
-            courses,
-
+            },
+            'AREA A: FOUNDATIONS OF COMMUNICATION<br>': (line, i, lines) => {
+                const required = parseInt(rmbr(lines[i - 5]))
+                const applied = parseInt(rmbr(lines[i - 2]))
+                obj.core_credits_required = required
+                obj.core_credits_applied = applied
+            },
+            
         }
-        console.log("Transformed Successfully!")
+        text.split('\n').forEach((line, i, lines) => {
+            console.log(i, line)
+            if (line in searchFor) {
+                searchFor[line](line, i, lines)
+            }
+        })
+
         return obj
-    }).then(results => write(path.resolve(__dirname, './courses.json'), results).then(() => results)).catch((err) => { console.log("welp something went wrong", err) })
+    }).then(results => write(path.resolve(__dirname, './degree-eval.json'), results).then(() => results)).catch((err) => { console.log("welp something went wrong", err) })
 }
 
 if (!module.parent) {
     //scraper(()=>loadFile('./all-classes.html'))
-    scraper(() => loadFile(path.resolve(__dirname, './all-classes.html')))
+    scraper(() => loadFile(path.resolve(__dirname, FILE_NAME)))
 }
 module.exports = () => scraper(() => getHTML())
 module.exports.scraper = scraper
-module.exports.loadFile = () => scraper(() => loadFile(path.resolve(__dirname, './all-classes.html')))
+module.exports.loadFile = () => scraper(() => loadFile(path.resolve(__dirname, FILE_NAME)))
