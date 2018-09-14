b6e57a799705b0249d6cc56632469978a4a4eda5
Nick Perez <nperez0111@gmail.com>
2018-09-14 13:19:54 -0700

    pretty print

--- usf/Proffessors/scrape-browser.js
+++ usf/Proffessors/scrape-browser.js
@@ -1,5 +1,25 @@
 const puppeteer = require('puppeteer'),
-    pagesToScrape = ['https://www.usfca.edu/arts-sciences/faculty', 'https://www.usfca.edu/law/faculty/full-time', 'https://www.usfca.edu/law/faculty/visiting', 'https://www.usfca.edu/law/faculty/adjunct', 'https://www.usfca.edu/law/faculty/emeriti', 'https://www.usfca.edu/management/about/faculty/alphabetical-list', 'https://www.usfca.edu/nursing/about-the-school/faculty', 'https://www.usfca.edu/education/programs/masters-credential-programs/catholic-educational-leadership/faculty', 'https://www.usfca.edu/education/educational-technology/faculty', 'https://www.usfca.edu/education/programs/masters-credential-programs/higher-education-student-affairs/faculty', 'https://www.usfca.edu/education/programs/masters-credential-programs/human-rights-education/faculty', 'https://www.usfca.edu/education/programs/masters-credential-programs/international-multicultural-education/faculty', 'https://www.usfca.edu/education/programs/doctoral-programs/learning-instruction/faculty', 'https://www.usfca.edu/education/programs/masters-credential-programs/marriage-family-therapy/faculty', 'https://www.usfca.edu/education/programs/masters-credential-programs/organization-leadership/faculty', 'https://www.usfca.edu/education/programs/masters-credential-programs/school-counseling/faculty', 'https://www.usfca.edu/education/programs/masters-credential-programs/special-education/faculty', 'https://www.usfca.edu/education/programs/masters-credential-programs/teaching/faculty', 'https://www.usfca.edu/education/programs/masters-credential-programs/tesol/faculty'],
+    pagesToScrape = [
+        'https://www.usfca.edu/arts-sciences/faculty',
+        'https://www.usfca.edu/law/faculty/full-time',
+        'https://www.usfca.edu/law/faculty/visiting',
+        'https://www.usfca.edu/law/faculty/adjunct',
+        'https://www.usfca.edu/law/faculty/emeriti',
+        'https://www.usfca.edu/management/about/faculty/alphabetical-list',
+        'https://www.usfca.edu/nursing/about-the-school/faculty',
+        'https://www.usfca.edu/education/programs/masters-credential-programs/catholic-educational-leadership/faculty',
+        'https://www.usfca.edu/education/educational-technology/faculty',
+        'https://www.usfca.edu/education/programs/masters-credential-programs/higher-education-student-affairs/faculty',
+        'https://www.usfca.edu/education/programs/masters-credential-programs/human-rights-education/faculty',
+        'https://www.usfca.edu/education/programs/masters-credential-programs/international-multicultural-education/faculty',
+        'https://www.usfca.edu/education/programs/doctoral-programs/learning-instruction/faculty',
+        'https://www.usfca.edu/education/programs/masters-credential-programs/marriage-family-therapy/faculty',
+        'https://www.usfca.edu/education/programs/masters-credential-programs/organization-leadership/faculty',
+        'https://www.usfca.edu/education/programs/masters-credential-programs/school-counseling/faculty',
+        'https://www.usfca.edu/education/programs/masters-credential-programs/special-education/faculty',
+        'https://www.usfca.edu/education/programs/masters-credential-programs/teaching/faculty',
+        'https://www.usfca.edu/education/programs/masters-credential-programs/tesol/faculty'
+    ],
     waitForLoad = (page) => new Promise((resolve) => {
         page.on('rquest', (req) => {
             waitForLoad(page)
@@ -27,13 +47,13 @@ function getPage(link) {
 }
 
 let scrape = async() => {
-    
+
     console.log("Scraping with the browser...")
     //handlePage Promise<Promise<Array<Promise<OBJ>>>>
     const AllProfessors = await (Promise.all(pagesToScrape.slice(0).map(async function handlePage(pageToScrape) {
         const page = getPage(pageToScrape);
         //proffesors Promise<Array<Promise<OBJ>>>
-        const proffesors = getLinks(page).then(links => links.map(async function(link) {
+        const proffesors = getLinks(page).then(links => links.map(async function (link) {
             try {
                 return scrapeProfesor(getPage(link), link)
             } catch (e) {
@@ -47,13 +67,13 @@ let scrape = async() => {
         if (linkToNextPage) {
             //const itsProffesors = await (await handlePage(linkToNextPage))
             //itsProffesors Array<Promise<OBJ>>
-            return Promise.resolve((await Promise.all(await proffesors)).concat(await (await handlePage("https://www.usfca.edu"+linkToNextPage))))
+            return Promise.resolve((await Promise.all(await proffesors)).concat(await (await handlePage("https://www.usfca.edu" + linkToNextPage))))
         }
         return proffesors
 
     })).then(async(allProffessors) => (await Promise.all(allProffessors.map(proffesors => Promise.all(proffesors)))).reduce((prev, curr) => prev.concat(curr).filter(a => a), [])));
     console.log("Scraped All Successfully!");
-    
+
     return AllProfessors
 }
 if (!module.parent) {
