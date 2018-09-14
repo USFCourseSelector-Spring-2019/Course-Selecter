ccb1028715c6e54c4f9d26e935f74c9590ef1ee2
Nick Perez <nperez0111@gmail.com>
2018-09-14 12:03:25 -0700

    added parsing of that one weird line

--- usf/Degree Evaluations/index.js
+++ usf/Degree Evaluations/index.js
@@ -17,7 +17,39 @@ function toCourseNames(line, i, lines) {
     const classes = []
     const result = /([\d]+) (?:Class(?:es)?)? in ([A-Z]+) (\d+)(?:\*)?(?: and ([\d]+))*(?: or @ @ (?:ATTRIBUTE = (\w+)))*/.exec(line)
     if (result === null) {
-        classes.push(line)
+        //"4 Credits in BIOL 300:499 or 598 or 599* or ENVS 410*"
+        const [first, ...rest] = line.split(" or ")
+        const creditLine = /(?:([\d]+) Credits in )([\w]+) ([\d]+):([\d]+)/g.exec(first)
+        if (creditLine === null) {
+            classes.push({ CRN: line, name: rmbr(lines[i - 1]) })
+        } else {
+            let [entire, numberOfCredits, subject_prefix, first_number, second_number] = creditLine
+            classes.push({
+                CRN: {
+                    from: subject_prefix + first_number,
+                    to: subject_prefix + second_number,
+                    subject: subject_prefix,
+                    start: first_number,
+                    end: second_number,
+                    credits: numberOfCredits
+                },
+                name: numberOfCredits + ' credits of ' + rmbr(lines[i - 1]),
+                credits: numberOfCredits
+            })
+            rest.forEach(course => {
+                const courseLine = /(?:([\w]+) )?([\d]+)/g.exec(course)
+                if (courseLine) {
+                    const [wholeThing, new_prefix, course_number] = courseLine
+                    if (new_prefix) {
+                        subject_prefix = new_prefix
+                    }
+                    classes.push({
+                        CRN: subject_prefix + course_number,
+                        name: subject_prefix + course_number
+                    })
+                }
+            })
+        }
     } else {
         const [whole, numberOfClasses, subject_prefix, first_num, second_num, or_attribute] = result
         if (numberOfClasses === '1') {
