692dd044e7e34930b74386f8db151982f6327b88
Nick Perez <nperez0111@gmail.com>
2018-09-16 23:12:51 -0700

    got some course card working and a bit prettier of ui

---
+++ client/components/CourseCard.vue
@@ -0,0 +1,112 @@
+<template>
+    <v-card :class="`full-height pa-2 ma-0 layout column justify-space-between ${available?'enabled':'disabled'}`" :color="available?'white':'grey lighten-3 grey--text text--darken-1'" v-on:click="available?(()=>{openCourseInfo=true}):undefined" :ripple="available">
+        <v-flex xs3 md6>
+            <h1 v-text="title" class="headline mb-3 mt-2 break-word"></h1>
+        </v-flex>
+        <h2 v-text="shortcode+id" class="title mb-3 font-weight-regular"></h2>
+        <h3 v-text="instructor" class="title mb-3 font-weight-regular"></h3>
+        <v-layout justify-space-between align-center row wrap class="px-0 no-grow ma-0">
+            <h4 v-text="days.join('')" class="title font-weight-regular"></h4>
+            <h4 v-text="times.map(timeFilter).join(' - ')" class="title my-2 font-weight-regular"></h4>
+        </v-layout>
+    </v-card>
+</template>
+<script>
+export default {
+    data() {
+            return {
+                adding: false,
+                professor: false,
+                openCourseInfo: false
+            }
+        },
+        mounted() {
+            /*
+            this.$api.courses.getProfessorData({
+                params: {
+                    professor_name: this.course.instructor
+                }
+            }).then(professorData => {
+                this.professor = professorData
+            }).catch(err => {
+                this.professor = false
+            })*/
+        },
+        props: {
+            available: Boolean,
+            crn: String,
+            shortcode: String,
+            id: String,
+            section: String,
+            campus: String,
+            credits: String,
+            title: String,
+            days: Array,
+            times: Array,
+            capacity: Number,
+            enrolled: Number,
+            remaining: Number,
+            "wl_remaing": String,
+            instructor: String,
+            dates: Array,
+            loc: String,
+            attributes: Array,
+            subject: String,
+            index: Number,
+            showAdded: {
+                default: true,
+                type: Boolean
+            }
+        },
+        methods: {
+            addCourse() {
+                this.adding = true
+                this.$store.commit('planner/addCourse', this.course)
+                setTimeout(() => (this.adding = false), 500)
+            },
+            timeFilter(time) {
+                if (time.startsWith("0")) {
+                    time = time.slice(1)
+                }
+                return time.slice(0, -3) + time.slice(-2).toUpperCase()
+            }
+        },
+        computed: {
+            image() {
+                const professor = this.professor
+                if (professor) {
+                    return professor.images[0]
+                }
+                return 'https://image.shutterstock.com/mosaic_250/0/0/518740741.jpg'
+            },
+            bio() {
+                const professor = this.professor
+                if (professor) {
+                    return professor.bio.join('\n')
+                }
+            },
+            link() {
+                const professor = this.professor
+                if (professor) {
+                    return professor.link
+                }
+            },
+            inPlanner() {
+                return this.$store.getters['planner/isInPlan'](this.course)
+            }
+        },
+}
+</script>
+<style>
+.enabled {
+    cursor: pointer;
+}
+
+.no-grow {
+    flex-grow: 0 !important;
+}
+
+.break-word {
+    word-wrap: break-word;
+}
+</style>
--- client/components/Subject.vue
+++ client/components/Subject.vue
@@ -1,48 +1,65 @@
 <template>
-    <v-card raised class="mx-1 my-3">
-        <v-card-title class="primary-fg--text primary layout row">
-            <h3 v-text="subject.subject" class="display-1"></h3>
-            <v-spacer></v-spacer>
-            <h3 v-text="subject.shortcode" class="title"></h3>
-        </v-card-title>
-        <v-data-table :headers="headers" :items="items" hide-actions item-key="index" expand>
-            <template slot="headerCell" slot-scope="props">
-                <v-tooltip bottom :disabled="!props.header.description">
-                    <span slot="activator" class="secondary-fg--text">
+    <div>
+        <v-card raised class="mx-1 my-3" v-if="$vuetify.breakpoint.lgAndUp">
+            <v-card-title class="primary-fg--text primary layout row">
+                <h3 v-text="subject.subject" class="display-1"></h3>
+                <v-spacer></v-spacer>
+                <h3 v-text="subject.shortcode" class="title"></h3>
+            </v-card-title>
+            <v-data-table :headers="headers" :items="courses" hide-actions item-key="index" expand>
+                <template slot="headerCell" slot-scope="props">
+                    <v-tooltip bottom :disabled="!props.header.description">
+                        <span slot="activator" class="secondary-fg--text">
                   {{ props.header.text }}
                 </span>
-                    <span>
+                        <span>
                   {{ props.header.description }}
                 </span>
-                </v-tooltip>
-            </template>
-            <template slot="items" slot-scope="props">
-                <tr @click="props.expanded = !props.expanded, goTo($refs[`row-${props.item.index}`])" :ref="`row-${props.item.index}`" :class="{'red darken-4 white--text':!props.item.available,'datatable__expand-row':!props.item.available,enabled:props.item.available}" :key="expand(props).item.index" :id="props.item.index">
-                    <td>{{ props.item.title }}</td>
-                    <td class="text-xs-center">{{ props.item.shortcode }} {{props.item.id}}</td>
-                    <td class="text-xs-left">{{ props.item.instructor }}</td>
-                    <td class="text-xs-center">{{ props.item.days.join('') }}</td>
-                    <td class="text-xs-left">{{ props.item.times.join(' - ') }}</td>
-                    <td class="text-xs-center">{{ props.item.credits }}</td>
-                    <td class="text-xs-center">{{ props.item.enrolled }}</td>
-                    <td class="text-xs-center">{{ props.item.remaining }}</td>
-                    <td class="text-xs-center">{{ props.item.wl_remaining }}</td>
-                </tr>
-            </template>
-            <template slot="expand" slot-scope="props">
-                <Course :course="props.item" @close="props.expanded=0" v-if="props.item.available && props.expanded" />
-                <v-alert :value="props.item.available===false" color="error" icon="warning" v-else>
-                    Sorry {{props.item.title}} on {{props.item.days.join('')}} with {{props.item.instructor}} at {{props.item.times.join(" - ")}} is closed. Even the wait list is full or closed off.
+                    </v-tooltip>
+                </template>
+                <template slot="items" slot-scope="props">
+                    <tr @click="props.expanded = !props.expanded, goTo($refs[`row-${props.item.index}`])" :ref="`row-${props.item.index}`" :class="{'grey lighten-4 grey--text text--darken-1 fix-border':!props.item.available,'datatable__expand-row':!props.item.available,enabled:props.item.available}" :key="expand(props).item.index" :id="props.item.index">
+                        <td>{{ props.item.title }}</td>
+                        <td class="text-xs-center">{{ props.item.shortcode }} {{props.item.id}}</td>
+                        <td class="text-xs-left">{{ props.item.instructor }}</td>
+                        <td class="text-xs-center">{{ props.item.days.join('') }}</td>
+                        <td class="text-xs-left">{{ props.item.times.join(' - ') }}</td>
+                        <td class="text-xs-center">{{ props.item.credits }}</td>
+                        <td class="text-xs-center">{{ props.item.enrolled }}</td>
+                        <td class="text-xs-center">{{ props.item.remaining }}</td>
+                        <td class="text-xs-center">{{ props.item.wl_remaining }}</td>
+                    </tr>
+                </template>
+                <template slot="expand" slot-scope="props">
+                    <Course :course="props.item" @close="props.expanded=0" v-if="props.item.available && props.expanded" />
+                    <v-alert :value="props.item.available===false" color="red darken-4" icon="warning" v-else @click="props.expanded=false" class="my-0">
+                        Sorry {{props.item.title}} on {{props.item.days.join('')}} with {{props.item.instructor}} at {{props.item.times.join(" - ")}} is closed. Even the wait list is full or closed off.
+                        <v-icon class="right" color="white" @click.stop="props.expanded=false">close</v-icon>
+                    </v-alert>
+                </template>
+                <v-alert slot="no-results" :value="true" color="error" icon="warning">
+                    Your search found no results.
                 </v-alert>
-            </template>
-            <v-alert slot="no-results" :value="true" color="error" icon="warning">
-                Your search found no results.
-            </v-alert>
-        </v-data-table>
-    </v-card>
+            </v-data-table>
+        </v-card>
+        <v-container v-else>
+            <v-layout justify-space-between row>
+                <v-subheader v-text="subject.subject" class="px-0 title"></v-subheader>
+                <v-subheader v-text="subject.shortcode" class="px-0 title"></v-subheader>
+            </v-layout>
+            <v-container fluid grid-list-md class="pa-0">
+                <v-layout row wrap>
+                    <v-flex v-for="course in courses" :key="course.index" xs6 sm4 md3 xl2>
+                        <course-card v-bind="course"></course-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-container>
+    </div>
 </template>
 <script>
 import Course from './Course'
+import CourseCard from './CourseCard'
 export default {
     data() {
             return {
@@ -108,7 +125,7 @@ export default {
         },
         props: ['subject'],
         computed: {
-            items() {
+            courses() {
                 return this.subject.courses.reduce((arr, course) => {
                     const classes = course.classes
                     classes.forEach(classy => {
@@ -120,11 +137,12 @@ export default {
             }
         },
         components: {
-            Course
+            Course,
+            CourseCard
         },
         methods: {
             expand(props) {
-                if (this.items.length === 1 && props.expanded !== 0) {
+                if (this.courses.length === 1 && props.expanded !== 0) {
                     props.expanded = true
                 }
                 return props
@@ -150,5 +168,15 @@ tr.enabled {
 
 .theme--dark .table tbody tr:nth-last-child(2) {
     border-bottom: none;
+}
+
+@media only screen and (min-width: 960px) {
+    .container {
+        max-width: unset !important;
+    }
+}
+
+.theme--light .v-table tbody tr.fix-border {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12) !important;
 }
 </style>
