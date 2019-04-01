36780fa54cffa9359ee9a5da4be24bedfb79c87e
Nick Perez <nperez0111@gmail.com>
2019-03-31 22:03:15 -0700

    Slight refactor of backend to start server

--- client/api_helpers/index.js
+++ client/api_helpers/index.js
@@ -1,43 +1,38 @@
-const constants=require('./constants'),
-cookieParser = require('cookie-parser'),
-    jwt = require('express-jwt');
-    import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'
+import cookieParser from 'cookie-parser'
+import jwt from 'express-jwt'
+import constant from './constants'
 
-async function getUser(request) {
-    return new Promise((resolve, reject) => {
-        const doTheThing=() => {
-                request.headers.authorization = request.cookies['auth._token.' + request.cookies['auth.strategy']]
-                if (request.headers.authorization === 'false') {
-                    return reject('No Auth token found')
-                }
-                jwt({
-                    secret: constants.secret
-                })(request, request.res, err => {
-                    if (err) {
-                        reject(err)
-                    }
-                    resolve(request.user)
-                })
-            }
-        if (!request.cookies) {
-            cookieParser()(request, request.res, doTheThing)
-        } else {
-            doTheThing()
+export async function getUser (request) {
+  return new Promise((resolve, reject) => {
+    const doTheThing = () => {
+      request.headers.authorization =
+        request.cookies[`auth._token.${request.cookies['auth.strategy']}`]
+      if (request.headers.authorization === 'false') {
+        return reject(new Error('No Auth token found'))
+      }
+      jwt({
+        secret: constants.secret
+      })(request, request.res, err => {
+        if (err) {
+          reject(err)
         }
-    })
-
+        resolve(request.user)
+      })
+    }
+    if (!request.cookies) {
+      cookieParser()(request, request.res, doTheThing)
+    } else {
+      doTheThing()
+    }
+  })
 }
 
-async function hashPassword(password){
-    return bcrypt.hash(password, constants.saltRounds)
+export async function hashPassword (password) {
+  return bcrypt.hash(password, constants.saltRounds)
 }
-async function checkPassword(password, hash){
-    return bcrypt.compare(password, hash)
+export async function checkPassword (password, hash) {
+  return bcrypt.compare(password, hash)
 }
 
-module.exports = {
-    constants,
-    getUser,
-    hashPassword,
-    checkPassword
-}
+export const constants = constant
