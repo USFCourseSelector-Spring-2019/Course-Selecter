4956d110b19cc619b55753266abd6b5039d5d6d4
namiranianp <namiranianp@gmail.com>
2019-04-26 02:34:01 -0700

    made progress on custom string for ics

--- client/store/planner.js
+++ client/store/planner.js
@@ -87,39 +87,48 @@ export const actions = {
     const FileSaver = require('file-saver')
     const moment = extendMoment(Moment)
     const plan_to_download = plans[index]
-    const ics = require('ics')
     const day_list = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA']
     const semester_year = (plan_to_download.courses[0].dates[0].slice(0, 2) < moment().month()) ? moment().year() + 1 : moment().year()
-    const march_dls = moment(semester_year + '-03-14')
-    const nov_dls = moment(semester_year + '-11-07')
-    let icsEvents = []
 
-    //this sets up daylight savings
-    while(march_dls.day() != 0) {
-      march_dls.subtract(1, 'days')
-    }
-    while(nov_dls.day() != 0) {
-      nov_dls.subtract(1, 'days')
-    }
-
-    console.log('march_dls',march_dls.month(), march_dls.date())
+    //required iCalendar info
+    let calendar_string = 'BEGIN:VCALENDAR\nPRODID:-//Google Inc//Google Calendar 70.9054//EN\nVERSION:2.0\nCALSCALE:GREGORIAN'
+    calendar_string += '\nMETHOD:PUBLISH\nX-WR-CALNAME:test' + plan_to_download.title
+    //timezone and daylight savings info
+    calendar_string += '\nX-WR-TIMEZONE:America/Los_Angeles\nBEGIN:VTIMEZONE\nTZID:America/Los_Angeles'
+    calendar_string += '\nX-LIC-LOCATION:America/Los_Angeles\nBEGIN:DAYLIGHT\nTZOFFSETFROM:-0800\nTZOFFSETTO:-0700\nTZNAME:PDT'
+    calendar_string += '\nDTSTART:19700308T020000\nRRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU\nEND:DAYLIGHT\nBEGIN:STANDARD\nTZOFFSETFROM:-0700'
+    calendar_string += '\nTZOFFSETTO:-0800\nTZNAME:PST\nDTSTART:19701101T020000\nRRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU\nEND:STANDARD'
+    calendar_string += '\nEND:VTIMEZONE\n'
 
     plan_to_download.courses.forEach(function(plan_course) {
       console.log(plan_course)
-
       let start_moment = moment(plan_course.dates[0] + '/' + semester_year)
       let end_moment = moment(plan_course.dates[1] + '/' + semester_year)
-      let march_dls_copy = march_dls.clone()
-      let nov_dls_copy = nov_dls.clone()
 
       start_moment.hour(Number(plan_course.times[0].slice(0,2)))
       start_moment.minute(Number(plan_course.times[0].slice(3,5)))
       end_moment.hour(Number(plan_course.times[1].slice(0,2)))
       end_moment.minute(Number(plan_course.times[1].slice(3,5)))
 
-      let recurr_string = 'FREQ=WEEKLY;BYDAY='
-      let course_dows = []
+      if (plan_course.times[0].slice(-2) == 'pm' && start_moment.hours() != 12) {
+        start_moment.add(12, 'hours')
+      }
+      if (plan_course.times[1].slice(-2) == 'pm' && end_moment.hours() != 12) {
+        end_moment.add(12, 'hours')
+      }
 
+      let duration_minute = 0
+      let duration_hour = 0
+
+      if (start_moment.minutes() > end_moment.minutes()) {
+        duration_minute = (60 + end_moment.minutes()) - start_moment.minutes()
+        duration_hour = (end_moment.hours() - 1) - start_moment.hours()
+      } else {
+        duration_minute = end_moment.minutes() - start_moment.minutes()
+        duration_hour = end_moment.hours()  - start_moment.hours()
+      }
+
+      let course_dows = []
       plan_course.days.forEach(function(day) {
         switch(day) {
           case 'M':
@@ -146,29 +155,40 @@ export const actions = {
         }
       })
 
-      if (plan_course.times[0].slice(-2) == 'pm' && start_moment.hours() != 12) {
-        start_moment.add(12, 'hours')
-      }
-      if (plan_course.times[1].slice(-2) == 'pm' && end_moment.hours() != 12) {
-        end_moment.add(12, 'hours')
-      }
-
-      let duration_minute = 0
-      let duration_hour = 0
-
-      if (start_moment.minutes() > end_moment.minutes()) {
-        duration_minute = (60 + end_moment.minutes()) - start_moment.minutes()
-        duration_hour = (end_moment.hours() - 1) - start_moment.hours()
-      } else {
-        duration_minute = end_moment.minutes() - start_moment.minutes()
-        duration_hour = end_moment.hours()  - start_moment.hours()
-      }
-
       //there is an issue with the first day of the semester being later than when the first day of this class is
       //in this case we increment the start date to match the first actual class and not the start of the semester
       while(!course_dows.includes(start_moment.day())) {
         start_moment.add(1, 'day')
       }
+
+      calendar_string += 'BEGIN:VEVENT\nDTSTART;TZID=America/Los_Angeles:'
+      calendar_string += start_moment.year()
+      calendar_string += ((start_moment.month() + 1).toString().length == 2) ? start_moment.month() + 1 : '0' + (start_moment.month() + 1)
+      calendar_string += (start_moment.date().toString().length == 2) ? start_moment.date() : '0' + start_moment.date()
+      calendar_string += 'T'
+      calendar_string += (start_moment.hours().toString().length == 2) ? start_moment.hours() : '0' + start_moment.hours()
+      calendar_string += (start_moment.minutes().toString().length == 2) ? start_moment.minutes() : '0' + start_moment.minutes()
+      calendar_string += '00'
+      calendar_string += '\nDTEND;TZID=America/Los_Angeles:'
+      calendar_string += start_moment.year()
+      calendar_string += ((start_moment.month() + 1).toString().length == 2) ? start_moment.month() : '0' + (start_moment.month() + 1)
+      calendar_string += (start_moment.date().toString().length == 2) ? start_moment.date() : '0' + start_moment.date()
+      calendar_string += 'T'
+      calendar_string += (end_moment.hours().toString().length == 2) ? end_moment.hours() : '0' + end_moment.hours()
+      calendar_string += (end_moment.minutes().toString().length == 2) ? end_moment.minutes() : '0' + end_moment.minutes()
+      calendar_string += '00'
+      calendar_string += '\nRRULE:FREQ=WEEKLY;WKST=SU;UNTIL=20200509T065959Z;BYDAY=MO,WE'
+      calendar_string += '\nDTSTAMP:20190425T223640Z\nUID:7q3thetls50ginkogm9qhg0o97@google.com\nCREATED:20190425T223609Z\nDESCRIPTION:'
+      calendar_string += '\nLAST-MODIFIED:20190425T223609Z\nLOCATION:\nSEQUENCE:0\nSTATUS:CONFIRMED\nSUMMARY:test course\nTRANSP:OPAQUE'
+      calendar_string += '\nEND:VEVENT\n'
+
+      return      
+
+      let recurr_string = 'FREQ=WEEKLY;BYDAY='
+
+      
+
+      
 
       //ICS uses UTC time so it is 8 hours ahead of SF time (PST)
       //this is here to correct for the repeat days
@@ -185,75 +205,12 @@ export const actions = {
           recurr_string += day_list[day_num] + ','
         })
       }
-
-      //we need 2 separate ICS events because daylight savings messes everything up
-      if(start_moment < march_dls < end_moment) {
-        march_dls_copy.minutes(start_moment.minutes())
-        march_dls_copy.hours(start_moment.hours())
-        recurr_string = recurr_string.slice(0, -1)
-        let dls_recurr_string = recurr_string
-
-        //before daylight savings
-        recurr_string += ';INTERVAL=1;UNTIL='
-        recurr_string += march_dls.year()
-        recurr_string += ((march_dls.month() + 1).toString().length == 2) ? march_dls.month() + 1 : '0' + (march_dls.month() + 1)
-        recurr_string += (march_dls.date().toString().length == 2) ? march_dls.date() : '0' + march_dls.date()
-        recurr_string += 'T070000Z'
-        //don't want to just convert from local to UTC, because it is SF time to UTC
-        //before DLS the difference between SF and UTC is 7 hrs
-        start_moment.add(8, 'hours')
-        let course_ics_event = {
-          title: plan_course.title,
-          start: [start_moment.year(), start_moment.month() + 1, start_moment.date(), start_moment.hours(), start_moment.minutes()],
-          duration: { hours: duration_hour, minutes: duration_minute },
-          location: plan_course.loc,
-          recurrenceRule: recurr_string
-        }
-        icsEvents.push(course_ics_event)
-
-        //after daylight savings
-        march_dls_copy.add(8, 'hours')
-        while(course_dows[0] != march_dls_copy.day()) {
-          march_dls_copy.add(1, 'day')
-        }
-
-        dls_recurr_string += ';INTERVAL=1;UNTIL='
-        dls_recurr_string += end_moment.year() 
-        dls_recurr_string += ((end_moment.month() + 1).length == 2) ? end_moment.month() + 1 : '0' + (end_moment.month() + 1)
-        dls_recurr_string += (end_moment.date().length == 2) ? end_moment.date() : '0' + end_moment.date()
-        dls_recurr_string += 'T070000Z'
-        course_ics_event = {
-          title: plan_course.title,
-          start: [march_dls_copy.year(), parseInt(march_dls_copy.month() + 1), march_dls_copy.date(), march_dls_copy.hours(), march_dls_copy.minutes()],
-          duration: { hours: duration_hour, minutes: duration_minute },
-          location: plan_course.loc,
-          recurrenceRule: dls_recurr_string
-        }
-        // icsEvents.push(course_ics_event)
-      } else if(start_moment < nov_dls < end_moment) {
-        recurr_string = recurr_string.slice(0, -1)
-        let recurr_string_copy = recurr_string
-        recurr_string += ';INTERVAL=1;UNTIL='
-        recurr_string += end_moment.year() 
-        recurr_string += ((end_moment.month() + 1).length == 2) ? end_moment.month() + 1 : '0' + (end_moment.month() + 1)
-        recurr_string += (end_moment.date().length == 2) ? end_moment.date() : '0' + end_moment.date()
-        recurr_string += 'T070000Z'
-
-        console.log("hitler was aight")
-      }
     })
+    calendar_string += 'END:VCALENDAR\n'
 
-    const { error, value } = ics.createEvents(icsEvents)
+    console.log(calendar_string)
 
-    if (error) {
-      console.log(error)
-      return
-    }
-
-    console.log(value)
-    // return
-
-    var blob = new Blob([value], {type: "text/plain;charset=utf-8"});
+    var blob = new Blob([calendar_string], {type: "text/plain;charset=utf-8"});
     FileSaver.saveAs(blob, plan_to_download.title + ".ics");
   },
   async showCourseView({ commit }) {
