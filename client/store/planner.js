705b80b1f50ec21229abae33c15f4466813d9df2
namiranianp <namiranianp@gmail.com>
2019-04-25 21:00:33 -0700

    pushing before I erase everything and start over

--- client/store/planner.js
+++ client/store/planner.js
@@ -2,7 +2,7 @@ import ls from 'local-storage'
 import { saveAs } from 'file-saver'
 import Moment from 'moment'
 import {
-    extendMoment
+  extendMoment
 } from 'moment-range'
 
 
@@ -12,10 +12,10 @@ function storeToLocalStorage ({ plans, plan }) {
 }
 
 const DEFAULT_PLANS = [
-  {
-    title: 'My 1st Planner',
-    courses: []
-  }
+{
+  title: 'My 1st Planner',
+  courses: []
+}
 ]
 
 export const state = () => ({
@@ -30,7 +30,7 @@ export const getters = {
   currentPlan: state => state.plans[state.plan],
   currentPlanIndex: state => state.plan,
   isInPlan: state => ({ index }) =>
-    state.plans[state.plan].courses.some(course => course.index === index),
+  state.plans[state.plan].courses.some(course => course.index === index),
   indexInPlan: state => ({ index }) => {
     let indexInPlan = -1
     state.plans[state.plan].courses.some((course, i) => {
@@ -83,187 +83,237 @@ export const mutations = {
 }
 
 export const actions = {
-    async downloadPlan({ state: { plans } }, { index }) {
-        const FileSaver = require('file-saver')
-        const moment = extendMoment(Moment)
-        const plan_to_download = plans[index]
-        const ics = require('ics')
-        const day_map = new Map([
-            ['M', 'MO'],
-            ['T', 'TU'],
-            ['W', 'WE'],
-            ['R', 'TH'],
-            ['F', 'FR'],
-            ['S', 'SA'],
-            ['U', 'SU']
-        ])
-        let icsEvents = []
+  async downloadPlan({ state: { plans } }, { index }) {
+    const FileSaver = require('file-saver')
+    const moment = extendMoment(Moment)
+    const plan_to_download = plans[index]
+    const ics = require('ics')
+    const day_list = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA']
+    const semester_year = (plan_to_download.courses[0].dates[0].slice(0, 2) < moment().month()) ? moment().year() + 1 : moment().year()
+    const march_dls = moment(semester_year + '-03-14')
+    const nov_dls = moment(semester_year + '-11-07')
+    let icsEvents = []
 
-        plan_to_download.courses.forEach(function(plan_course) {
-          console.log(plan_course)
-          const start_date = plan_course.dates[0]
-          const end_date = plan_course.dates[1]
-          const start_time = plan_course.times[0]
-          const end_time = plan_course.times[1]
+    //this sets up daylight savings
+    while(march_dls.day() != 0) {
+      march_dls.subtract(1, 'days')
+    }
+    while(nov_dls.day() != 0) {
+      nov_dls.subtract(1, 'days')
+    }
 
-          let start_day = Number(start_date.slice(start_date.indexOf('/') + 1))
-          let start_hour = Number(start_time.slice(0,2))
-          let end_hour = Number(end_time.slice(0,2))
-          let start_minute = Number(start_time.slice(3,5))
-          let end_minute = Number(end_time.slice(3,5))
-          let recurr_string = 'FREQ=WEEKLY;BYDAY='
+    console.log('march_dls',march_dls.month(), march_dls.date())
 
-          if (start_time.slice(-2) == 'pm' && start_hour != 12) {
-              start_hour += 12
-          }
-          if (end_time.slice(-2) == 'pm' && start_hour != 12) {
-              end_hour += 12
-          }
+    plan_to_download.courses.forEach(function(plan_course) {
+      console.log(plan_course)
 
-          console.log('start: ' + start_hour + ':' + start_minute)
-          console.log('end: ' + end_hour + ':' + end_minute)
+      let start_moment = moment(plan_course.dates[0] + '/' + semester_year)
+      let end_moment = moment(plan_course.dates[1] + '/' + semester_year)
+      let march_dls_copy = march_dls.clone()
+      let nov_dls_copy = nov_dls.clone()
 
-          let duration_minute = 0
+      start_moment.hour(Number(plan_course.times[0].slice(0,2)))
+      start_moment.minute(Number(plan_course.times[0].slice(3,5)))
+      end_moment.hour(Number(plan_course.times[1].slice(0,2)))
+      end_moment.minute(Number(plan_course.times[1].slice(3,5)))
 
-          if (start_minute > end_minute) {
-            duration_minute = (60 + end_minute) - start_minute
-            end_hour--
-          } else {
-            duration_minute = end_minute - start_minute
-          }
+      let recurr_string = 'FREQ=WEEKLY;BYDAY='
+      let course_dows = []
 
-          let duration_hour = end_hour - start_hour
-
-          //the time 0:00 is at 5pm the day before for ICS for some reason
-          //this is here to correct the actualy 24 hr time for ICS 24 hr time
-          if (start_hour > 17) {
-              start_hour -= 17
-              start_day++
-              plan_course.days.forEach(function(day) {
-                console.log("special days")
-                  switch(day) {
-                    case 'M':
-                      recurr_string += day_map.get('T')
-                      break;
-                    case 'T':
-                      recurr_string += day_map.get('W')
-                      break;
-                    case 'W':
-                      recurr_string += day_map.get('R')
-                      break;
-                    case 'R':
-                      recurr_string += day_map.get('F')
-                      break;
-                    case 'F':
-                      recurr_string += day_map.get('S')
-                      break;
-                    case 'S':
-                      recurr_string += day_map.get('U')
-                      break;
-                    case 'U':
-                      recurr_string += day_map.get('M')
-                      break;
-                  }
-                  recurr_string += ','
-              })  
-          } else {
-            start_hour += 8
-            plan_course.days.forEach(function(day) {
-                recurr_string += day_map.get(day) + ','
-                console.log("normal days: " + day_map.get(day))
-            })
-          }
-
-          console.log('start: ' + start_hour + ':' + start_minute)
-          console.log('end: ' + end_hour + ':' + end_minute)
-          
-          recurr_string = recurr_string.slice(0, -1)
-          recurr_string += ';INTERVAL=1;UNTIL='
-          recurr_string += moment().year() + end_date.slice(0, end_date.indexOf('/')) + end_date.slice(end_date.indexOf('/') + 1)
-          recurr_string += 'T070000Z'
-
-          let course_ics_event = {
-              title: plan_course.title,
-              start: [moment().year(), start_date.slice(0, start_date.indexOf('/')), start_day, start_hour, start_minute],
-              duration: { hours: duration_hour, minutes: duration_minute },
-              location: plan_course.loc,
-              recurrenceRule: recurr_string
-          }
-          icsEvents.push(course_ics_event)
+      plan_course.days.forEach(function(day) {
+        switch(day) {
+          case 'M':
+          course_dows.push(1)
+          break;
+          case 'T':
+          course_dows.push(2)
+          break;
+          case 'W':
+          course_dows.push(3)
+          break;
+          case 'R':
+          course_dows.push(4)
+          break;
+          case 'F':
+          course_dows.push(5)
+          break;
+          case 'S':
+          course_dows.push(6)
+          break;
+          case 'U':
+          course_dows.push(0)
+          break;
+        }
       })
 
-      const { error, value } = ics.createEvents(icsEvents)
-
-      if (error) {
-        console.log('error')
-        console.log(error)
-        return
+      if (plan_course.times[0].slice(-2) == 'pm' && start_moment.hours() != 12) {
+        start_moment.add(12, 'hours')
+      }
+      if (plan_course.times[1].slice(-2) == 'pm' && end_moment.hours() != 12) {
+        end_moment.add(12, 'hours')
       }
 
-      console.log(value)
-      // return
+      let duration_minute = 0
+      let duration_hour = 0
 
-      var blob = new Blob([value], {type: "text/plain;charset=utf-8"});
-      FileSaver.saveAs(blob, plan_to_download.title + ".ics");
-    },
-    async showCourseView({ commit }) {
-        await commit('setTab', 0)
-        await commit('showPlanner')
-    },
-    async showCalendarView({ commit }) {
-        await commit('setTab', 1)
-        await commit('showPlanner')
-    },
-    async saveSettings({ state, rootState: { auth: { loggedIn, user } } }, { $api }) {
-        //Store these settings to the user in the db and localstorage or just localstorage if not logged in
-        if (loggedIn) {
-            try {
-                console.log($api)
-                await $api.auth.savePlans({ body: { plans, plan } })
-            } catch (e) {
-                console.log('Failed to save plans with error:', e)
-            }
+      if (start_moment.minutes() > end_moment.minutes()) {
+        duration_minute = (60 + end_moment.minutes()) - start_moment.minutes()
+        duration_hour = (end_moment.hours() - 1) - start_moment.hours()
+      } else {
+        duration_minute = end_moment.minutes() - start_moment.minutes()
+        duration_hour = end_moment.hours()  - start_moment.hours()
+      }
+
+      //there is an issue with the first day of the semester being later than when the first day of this class is
+      //in this case we increment the start date to match the first actual class and not the start of the semester
+      while(!course_dows.includes(start_moment.day())) {
+        start_moment.add(1, 'day')
+      }
+
+      //ICS uses UTC time so it is 8 hours ahead of SF time (PST)
+      //this is here to correct for the repeat days
+      if (start_moment.hours() > 17) {
+        course_dows.forEach(function(day_num) {
+          if (day_num + 1 > 6) {
+            recurr_string += day_list[0] + ','            
+          } else {
+            recurr_string += day_list[day_num + 1] + ','
+          }
+        })  
+      } else {
+        course_dows.forEach(function(day_num) {
+          recurr_string += day_list[day_num] + ','
+        })
+      }
+
+      //we need 2 separate ICS events because daylight savings messes everything up
+      if(start_moment < march_dls < end_moment) {
+        march_dls_copy.minutes(start_moment.minutes())
+        march_dls_copy.hours(start_moment.hours())
+        recurr_string = recurr_string.slice(0, -1)
+        let dls_recurr_string = recurr_string
+
+        //before daylight savings
+        recurr_string += ';INTERVAL=1;UNTIL='
+        recurr_string += march_dls.year()
+        recurr_string += ((march_dls.month() + 1).toString().length == 2) ? march_dls.month() + 1 : '0' + (march_dls.month() + 1)
+        recurr_string += (march_dls.date().toString().length == 2) ? march_dls.date() : '0' + march_dls.date()
+        recurr_string += 'T070000Z'
+        //don't want to just convert from local to UTC, because it is SF time to UTC
+        //before DLS the difference between SF and UTC is 7 hrs
+        start_moment.add(8, 'hours')
+        let course_ics_event = {
+          title: plan_course.title,
+          start: [start_moment.year(), start_moment.month() + 1, start_moment.date(), start_moment.hours(), start_moment.minutes()],
+          duration: { hours: duration_hour, minutes: duration_minute },
+          location: plan_course.loc,
+          recurrenceRule: recurr_string
         }
-        storeToLocalStorage(state)
-    },
-    async addCourse({ commit, dispatch }, { payload, $api }) {
-        await commit('addCourse', payload)
-        await dispatch('saveSettings', { $api })
-    },
-    async removeCourse({ commit, dispatch }, { payload, $api }) {
-        await commit('removeCourseIndex', payload)
-        await dispatch('saveSettings', { $api })
-    },
-    async addPlan({ commit, dispatch }, { payload, $api }) {
-        await commit('addPlan', payload)
-        await dispatch('saveSettings', { $api })
-    },
-    async setTitleOf({ commit, dispatch }, { payload, $api }) {
-        await commit('setTitleOf', payload)
-        await dispatch('saveSettings', { $api })
-    },
-    async setCurPlan({ commit, dispatch }, { payload, $api }) {
-        await commit('setCurPlan', payload)
-        await dispatch('saveSettings', { $api })
-    },
-    async removePlan({ commit, dispatch }, { payload, $api }) {
-        await commit('removePlan', payload)
-        await dispatch('saveSettings', { $api })
-    },
-    async loadSettings({
-        commit,
-        rootState: { auth: { loggedIn, user } }
-    }, { $api }) {
-        const savedPlans = ls('plans'),
-            savedPlan = ls('plan')
-        if (loggedIn) {
-            const { plans, plan } = await $api.auth.getPlans()
-            await commit('setPlans', plans || savedPlan || DEFAULT_PLANS)
-            await commit('setCurPlan', plan || savedPlan || 0)
-        } else {
-            const savedPlans = ls('plans')
-            await commit('setPlans', savedPlans || DEFAULT_PLANS)
-            await commit('setCurPlan', savedPlan || 0)
+        icsEvents.push(course_ics_event)
+
+        //after daylight savings
+        march_dls_copy.add(8, 'hours')
+        while(course_dows[0] != march_dls_copy.day()) {
+          march_dls_copy.add(1, 'day')
         }
+
+        dls_recurr_string += ';INTERVAL=1;UNTIL='
+        dls_recurr_string += end_moment.year() 
+        dls_recurr_string += ((end_moment.month() + 1).length == 2) ? end_moment.month() + 1 : '0' + (end_moment.month() + 1)
+        dls_recurr_string += (end_moment.date().length == 2) ? end_moment.date() : '0' + end_moment.date()
+        dls_recurr_string += 'T070000Z'
+        course_ics_event = {
+          title: plan_course.title,
+          start: [march_dls_copy.year(), parseInt(march_dls_copy.month() + 1), march_dls_copy.date(), march_dls_copy.hours(), march_dls_copy.minutes()],
+          duration: { hours: duration_hour, minutes: duration_minute },
+          location: plan_course.loc,
+          recurrenceRule: dls_recurr_string
+        }
+        // icsEvents.push(course_ics_event)
+      } else if(start_moment < nov_dls < end_moment) {
+        recurr_string = recurr_string.slice(0, -1)
+        let recurr_string_copy = recurr_string
+        recurr_string += ';INTERVAL=1;UNTIL='
+        recurr_string += end_moment.year() 
+        recurr_string += ((end_moment.month() + 1).length == 2) ? end_moment.month() + 1 : '0' + (end_moment.month() + 1)
+        recurr_string += (end_moment.date().length == 2) ? end_moment.date() : '0' + end_moment.date()
+        recurr_string += 'T070000Z'
+
+        console.log("hitler was aight")
+      }
+    })
+
+    const { error, value } = ics.createEvents(icsEvents)
+
+    if (error) {
+      console.log(error)
+      return
+    }
+
+    console.log(value)
+    // return
+
+    var blob = new Blob([value], {type: "text/plain;charset=utf-8"});
+    FileSaver.saveAs(blob, plan_to_download.title + ".ics");
+  },
+  async showCourseView({ commit }) {
+    await commit('setTab', 0)
+    await commit('showPlanner')
+  },
+  async showCalendarView({ commit }) {
+    await commit('setTab', 1)
+    await commit('showPlanner')
+  },
+  async saveSettings({ state, rootState: { auth: { loggedIn, user } } }, { $api }) {
+    //Store these settings to the user in the db and localstorage or just localstorage if not logged in
+    if (loggedIn) {
+      try {
+        console.log($api)
+        await $api.auth.savePlans({ body: { plans, plan } })
+      } catch (e) {
+        console.log('Failed to save plans with error:', e)
+      }
+    }
+    storeToLocalStorage(state)
+  },
+  async addCourse({ commit, dispatch }, { payload, $api }) {
+    await commit('addCourse', payload)
+    await dispatch('saveSettings', { $api })
+  },
+  async removeCourse({ commit, dispatch }, { payload, $api }) {
+    await commit('removeCourseIndex', payload)
+    await dispatch('saveSettings', { $api })
+  },
+  async addPlan({ commit, dispatch }, { payload, $api }) {
+    await commit('addPlan', payload)
+    await dispatch('saveSettings', { $api })
+  },
+  async setTitleOf({ commit, dispatch }, { payload, $api }) {
+    await commit('setTitleOf', payload)
+    await dispatch('saveSettings', { $api })
+  },
+  async setCurPlan({ commit, dispatch }, { payload, $api }) {
+    await commit('setCurPlan', payload)
+    await dispatch('saveSettings', { $api })
+  },
+  async removePlan({ commit, dispatch }, { payload, $api }) {
+    await commit('removePlan', payload)
+    await dispatch('saveSettings', { $api })
+  },
+  async loadSettings({
+    commit,
+    rootState: { auth: { loggedIn, user } }
+  }, { $api }) {
+    const savedPlans = ls('plans'),
+    savedPlan = ls('plan')
+    if (loggedIn) {
+      const { plans, plan } = await $api.auth.getPlans()
+      await commit('setPlans', plans || savedPlan || DEFAULT_PLANS)
+      await commit('setCurPlan', plan || savedPlan || 0)
+    } else {
+      const savedPlans = ls('plans')
+      await commit('setPlans', savedPlans || DEFAULT_PLANS)
+      await commit('setCurPlan', savedPlan || 0)
     }
   }
+}
