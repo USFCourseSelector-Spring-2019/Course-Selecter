6fd3072321f6043e0c5b4161056a1171e9434406
Prescott  Carlson <prescottcarlson@Prescotts-MacBook-Pro.local>
2019-05-05 15:34:47 -0700

    Made changes to Planner Tab view

--- client/components/Planner.vue
+++ client/components/Planner.vue
@@ -1,127 +1,136 @@
 <template>
-    <div>
-        <v-toolbar color="primary" class="primary-fg--text" tabs>
-            <v-btn @click="$store.commit('planner/setTab',2)" icon color="primary-fg" flat>
-                <v-icon>settings</v-icon>
+<div>
+  <v-toolbar color="primary" class="primary-fg--text" tabs>
+    <v-btn @click="$store.commit('planner/setTab',2)" icon color="primary-fg" flat>
+      <v-icon>settings</v-icon>
+    </v-btn>
+    <v-toolbar-title>Plan: {{plan.title}}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon @click.native="$emit('close')" color="primary-fg" flat>
+      <v-icon>close</v-icon>
+    </v-btn>
+    <v-tabs slot="extension" centered v-model="curTab" slider-color="white" color="transparent" grow>
+      <v-tabs-slider color="secondary"></v-tabs-slider>
+      <v-tab ripple class="primary-fg--text">
+        Courses
+      </v-tab>
+      <v-tab ripple class="primary-fg--text">
+        Calendar
+      </v-tab>
+      <v-tab ripple class="primary-fg--text">
+        Settings
+      </v-tab>
+    </v-tabs>
+  </v-toolbar>
+  <v-tabs-items v-model="curTab">
+    <v-tab-item key="course">
+      <v-card style="container">
+        <v-layout column v-if="courses.length">
+          <v-flex v-for="(course,i) in courses" :key="course.index">
+            <v-card class="in-planner">
+              <Course :course="course" @close="$store.dispatch('planner/removeCourse',{payload:i,$api})" :show-added="false" />
+            </v-card>
+          </v-flex>
+        </v-layout>
+        <v-card-text v-else>
+          <h1 class="text-xs-center display-1 mb-5">Welcome to your Planner!</h1>
+          <p class="text-xs-center">This is where all the courses that you plan to take will be displayed. To begin your plan, add some courses!</p>
+          <v-layout justify-center align-center>
+            <v-btn class="text-xs-center primary-fg--text" color="primary" @click="goToCourses">
+              <v-icon left>add</v-icon>Add your first course !
             </v-btn>
-            <v-toolbar-title>Plan: {{plan.title}}</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon @click.native="$emit('close')" color="primary-fg" flat>
-                <v-icon>close</v-icon>
+          </v-layout>
+        </v-card-text>
+      </v-card>
+    </v-tab-item>
+    <v-tab-item key="calendar">
+      <v-card flat>
+        <Calendar :classes="courses" />
+      </v-card>
+    </v-tab-item>
+    <v-tab-item key="settings">
+      <v-card tile>
+        <v-card-title class="title">All Plans</v-card-title>
+        <v-subheader>You can create multiple possible schedules to see which is the best fit for you</v-subheader>
+        <v-card-text>
+          <v-layout column>
+            <v-layout v-for="(curPlan,i) in plans" :key="i">
+              <v-text-field :value="curPlan.title" :label="`Plan #${i+1}`" @change="title=>$store.dispatch('planner/setTitleOf',{payload:{title,index:i},$api})"></v-text-field>
+              <v-btn @click.native.stop="$store.dispatch('planner/setCurPlan',{payload:i,$api})" :disabled="$store.getters['planner/currentPlan']===curPlan" color="primary">
+                {{$store.getters['planner/currentPlan']===curPlan?'Is Current Plan':'Set as Current Plan'}}</v-btn>
+              <v-tooltip bottom>
+                <v-btn @click.native.stop="$store.dispatch('planner/removePlan',{payload:i,$api})" icon flat slot="activator">
+                  <v-icon>close</v-icon>
+                </v-btn>
+                <span>Delete {{curPlan.title}}</span>
+              </v-tooltip>
+            </v-layout>
+          </v-layout>
+          <v-tooltip top>
+            <v-btn @click.native.stop="$store.dispatch('planner/addPlan',{payload:{title: `Plan #${plans.length+1}`,courses: []},$api})" color="primary" slot="activator">
+              <v-icon left>add</v-icon>
+              Add A Plan
             </v-btn>
-            <template v-slot:extension>
-                <v-tabs centered v-model="curTab" slider-color="white" color="transparent" grow>
-                    <v-tabs-slider color="secondary"></v-tabs-slider>
-                    <v-tab ripple class="primary-fg--text" href="#tab-course">
-                        Courses
-                    </v-tab>
-                    <v-tab ripple class="primary-fg--text" href="#tab-calendar">
-                        Calendar
-                    </v-tab>
-                    <v-tab ripple class="primary-fg--text" href="#tab-settings">
-                        Settings
-                    </v-tab>
-                </v-tabs>
-            </template>
-        </v-toolbar>
-        <v-tabs-items v-model="curTab">
-            <v-tab-item key="course" value="tab-course">
-                <v-card flat>
-                    <v-layout column v-if="courses.length">
-                        <v-flex v-for="(course,i) in courses" :key="course.index">
-                            <Course :course="course" @close="$store.dispatch('planner/removeCourse',{payload:i,$api})" :show-added="false" />
-                        </v-flex>
-                    </v-layout>
-                    <v-card-text v-else>
-                        <h1 class="text-xs-center display-1 mb-5">Welcome to your Planner!</h1>
-                        <p class="text-xs-center">This is where all the courses that you plan to take will be displayed. To begin your plan, add some courses!</p>
-                        <v-layout justify-center align-center>
-                            <v-btn class="text-xs-center primary-fg--text" color="primary" @click="goToCourses">
-                                <v-icon left>add</v-icon>Add your first course !
-                            </v-btn>
-                        </v-layout>
-                    </v-card-text>
-                </v-card>
-            </v-tab-item>
-            <v-tab-item key="calendar" value="tab-calendar">
-                <v-card flat>
-                    <Calendar :classes="courses" />
-                </v-card>
-            </v-tab-item>
-            <v-tab-item key="settings" value="tab-settings">
-                <v-card tile>
-                    <v-card-title class="title">All Plans</v-card-title>
-                    <v-subheader>You can create multiple possible schedules to see which is the best fit for you</v-subheader>
-                    <v-card-text>
-                        <v-layout column>
-                            <v-layout v-for="(curPlan,i) in plans" :key="i">
-                                <v-text-field :value="curPlan.title" :label="`Plan #${i+1}`" @change="title=>$store.dispatch('planner/setTitleOf',{payload:{title,index:i},$api})"></v-text-field>
-                                <v-btn @click.native.stop="$store.dispatch('planner/setCurPlan',{payload:i,$api})" :disabled="$store.getters['planner/currentPlan']===curPlan" color="primary">{{$store.getters['planner/currentPlan']===curPlan?'Is Current Plan':'Set as Current Plan'}}</v-btn>
-                                <v-tooltip bottom v-if="canDeletePlans">
-                                    <template v-slot:activator="{ on }">
-                                        <v-btn @click.native.stop="$store.dispatch('planner/removePlan',{payload:i,$api})" icon flat v-on="on">
-                                            <v-icon>close</v-icon>
-                                        </v-btn>
-                                    </template>
-                                    <span>Delete {{curPlan.title}}</span>
-                                </v-tooltip>
-                            </v-layout>
-                        </v-layout>
-                        <v-tooltip top>
-                            <template v-slot:activator="{ on }">
-                                <v-btn @click.native.stop="$store.dispatch('planner/addPlan',{payload:{title: `Plan #${plans.length+1}`,courses: []},$api})" color="primary" v-on="on">
-                                    <v-icon left>add</v-icon>
-                                    Add A Plan
-                                </v-btn>
-                            </template>
-                            <span>Adds a clean possible plan to your list of plans</span>
-                        </v-tooltip>
-                    </v-card-text>
-                    <div style="flex: 1 1 auto;"></div>
-                </v-card>
-            </v-tab-item>
-        </v-tabs-items>
-    </div>
+            <span>Adds a clean possible plan to your list of plans</span>
+          </v-tooltip>
+        </v-card-text>
+        <div style="flex: 1 1 auto;"></div>
+      </v-card>
+    </v-tab-item>
+  </v-tabs-items>
+</div>
 </template>
 <script>
 import Course from './Course'
 import Calendar from './Calendar'
 
 export default {
-    data() {
-            return {
-                showSettings: false,
-            }
-        },
-        computed: {
-            curTab: {
-                get() {
-                    return this.$store.state.planner.curTab
-                },
-                set(index) {
-                    console.log('setting tab to', index)
-                    this.$store.commit('planner/setTab', index)
-                }
-            },
-            canDeletePlans(){
-                return this.$store.getters['planner/canDeletePlans']
-            }
-        },
-        methods: {
-            saveSettings() {
+  data() {
+    return {
+      showSettings: false,
+    }
+  },
+  computed: {
+    curTab: {
+      get() {
+        return this.$store.state.planner.curTab
+      },
+      set(index) {
+        console.log('setting tab to', index)
+        this.$store.commit('planner/setTab', index)
+      }
+    }
+  },
+  methods: {
+    saveSettings() {
 
-            },
-            goToCourses() {
-                if (this.$router.currentRoute.path == '/courses') {
-                    this.$store.commit('planner/hidePlanner')
-                } else {
-                    this.$router.push('/courses')
-                }
-            }
-        },
-        components: {
-            Course,
-            Calendar
-        }
+    },
+    goToCourses() {
+      if (this.$router.currentRoute.path == '/courses') {
+        this.$store.commit('planner/hidePlanner')
+      } else {
+        this.$router.push('/courses')
+      }
+    }
+  },
+  components: {
+    Course,
+    Calendar
+  }
 }
 </script>
+
+<style>
+.in-planner.v-card {
+  box-shadow: none;
+}
+
+.in-planner>.v-card {
+  /* add border radius and such to look like card*/
+  background-image: repeating-linear-gradient(white 0px, white 24px, #a9b1d0 25px);
+  border: 1px solid #ddd;
+  border-radius: 100%;
+  margin: 5px;
+  box-shadow: 0 0 2px 1px rgba(76, 90, 95, 0.13);
+}
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,33 +1,194 @@
+<!-- <template>
+<v-container>
+  <div class="image">
+    <img src="/mockup.png">
+  </div>
+  <div class="image1">
+    <img src="/iPhoneMockup.png">
+  </div>
+  <div class="hero-image">
+  </div>
+</v-container>
+</template> -->
+
+
+
 <template>
-    <v-container>
-        <h1 class="headline text-xs-center">Welcome!</h1>
-        <div class="text-xs-center">
-            <img src="/icon.png" alt="USF Logo" class="my-3" />
-        </div>
-        <v-card class="flex xs12 sm8 md6 mx-auto" to="/courses">
-            <h1 class="title text-xs-center font-weight-regular pt-3">We handle registration, you handle college</h1>
-            <v-card-text>
-                This website help you pick and plan your courses faster!
-            </v-card-text>
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" flat nuxt to="/courses">Search for Courses</v-btn>
-            </v-card-actions>
-        </v-card>
-        <v-card class="flex xs12 sm8 md6 mx-auto my-5">
-            <h1 class="title text-xs-center font-weight-regular pt-3">About</h1>
-            <v-card-text class="body-1">
-                Picking classes has to be one of the worst experiences in college. Constantly trying to make sure that you are progressing with your degree yet also juggling making sure that you have interesting (even fun if you are ambitious) classes while also creating a schedule that you won't dread for the whole semester. On top of all those things you have to keep in mind, you have to deal with the colleges terrible system for picking courses and have at least a million tabs open to the point that you forget which tab shows which little bit of info you need to make a reasonable schedule for a semester. It sure is a lot of pressure on you and the tools <span class="font-weight-bold">should</span> make the process easier not harder, so I made a course selector for USF students like you so that you can pick your courses with ease and still be on your way to your goal of graduating.
-            </v-card-text>
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" flat nuxt to="/about">Learn More</v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-container>
+<!-- <div class="fill-height layout wrap justify-center align-center">
+      <div class="fill-height layout justify-center align-center">
+          <v-card class="flex sign-in-card" raised>
+              <v-card-title>
+                  <h1 class="display-1">Sign In</h1>
+              </v-card-title>
+              <v-card-text>
+                  <v-form v-model="valid" ref="form" :class="{'has-error':error}" autocomplete="off">
+                      <v-text-field v-model="username" :rules="usernameRules" label="Username" type="text" required :error="error" v-focus clearable></v-text-field>
+                      <v-text-field v-model="password" :rules="passwordRules" label="Password" required :append-icon="passwordVisible ? 'visibility' : 'visibility_off'" @click:append="() => (passwordVisible = !passwordVisible)" :type="passwordVisible ? 'password' : 'text'" v-on:keyup.13="submit" :error="error" clearable></v-text-field>
+                      <v-layout align-center>
+                          <a href="/sign-up">Don't have an account? Sign Up!</a>
+                          <v-spacer></v-spacer>
+                          <v-btn :disabled="!valid" @click="submit" color="primary" ripple>
+                              Sign in
+                          </v-btn>
+                      </v-layout>
+                  </v-form>
+              </v-card-text>
+              <expander :expanded="error" style="position:absolute;left:0;right:0;">
+                  <v-alert :value="true" type="error">Invalid Username or Password... Please Try again!</v-alert>
+              </expander>
+          </v-card>
+      </div> -->
+<v-container>
+  <div class="LandingPageText">
+    <h1> Course Selecting,
+    </h1>
+    <h2> Like it should be.
+    </h2>
+  </div>
+  <div class="Mac">
+    <img style="width:100%; height:100%" src="/mockup.png">
+  </div>
+  <div class="iPhone">
+    <img style="width:100%; height:100%" src="/iPhoneMockup.png">
+  </div>
+  <div class="hero-image">
+  </div>
+  <!-- <v-card class="flex xs12 sm8 md6 mx-auto" to="/courses">
+    <h1 class="title text-xs-center font-weight-regular pt-3">We handle registration, you handle college</h1>
+    <v-card-text>
+      This website help you pick and plan your courses faster!
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" flat nuxt to="/courses">Search for Courses</v-btn>
+    </v-card-actions>
+  </v-card>
+  <v-card class="flex xs12 sm8 md6 mx-auto my-5">
+    <h1 class="title text-xs-center font-weight-regular pt-3">About</h1>
+    <v-card-text class="body-1">
+      Picking classes has to be one of the worst experiences in college. Constantly trying to make sure that you are progressing with your degree yet also juggling making sure that you have interesting (even fun if you are ambitious) classes while
+      also creating a schedule that you won't dread for the whole semester. On top of all those things you have to keep in mind, you have to deal with the colleges terrible system for picking courses and have at least a million tabs open to the point
+      that you forget which tab shows which little bit of info you need to make a reasonable schedule for a semester. It sure is a lot of pressure on you and the tools <span class="font-weight-bold">should</span> make the process easier not harder,
+      so I made a course selector for USF students like you so that you can pick your courses with ease and still be on your way to your goal of graduating.
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" flat nuxt to="/about">Learn More</v-btn>
+    </v-card-actions>
+  </v-card> -->
+</v-container>
 </template>
+
 <script>
 export default {
-    auth: false
+  auth: false
+  // data() {
+  //     return {
+  //         passwordVisible: true,
+  //         valid: false,
+  //         error: false,
+  //         username: '',
+  //         usernameRules: [
+  //             v => !!v || 'Username is required'
+  //         ],
+  //         password: '',
+  //         passwordRules: [
+  //             v => !!v || 'Password is required'
+  //         ]
+  //     }
+  // },
+  // layout: 'auth',
+  // methods: {
+  //     async submit() {
+  //         if (this.$refs.form.validate()) {
+  //             // Makes a POST Request to /login
+  //             // this.username and this.password are constantly in sync with the 'v-text-field' and stored in javascript
+  //             console.log("Trying to POST", JSON.stringify({
+  //                 username: this.username,
+  //                 password: this.password
+  //             }), `to: https://${document.location.hostname}:3000/auth/login`)
+  //             return this.$auth
+  //                 .loginWith('local', {
+  //                     data: {
+  //                         username: this.username,
+  //                         password: this.password
+  //                     }
+  //                 })
+  //                 .catch(e => {
+  //                     console.error(e)
+  //                     this.error = true
+  //                     this.valid = false
+  //                 }).then(() => {
+  //                     console.log('Signed in successfully!')
+  //                         //need to redirect if still on this page after
+  //                         //axios.defaults.headers.common['Authorization'] = this.$auth.$storage._state['_token.local']
+  //                     this.$router.push('/dashboard')
+  //                 })
+  //
+  //         } else {
+  //
+  //         }
+  //     }
+  // },
+  // watch: {
+  //     username() {
+  //         this.error = false
+  //     },
+  //     password() {
+  //         this.error = false
+  //     }
+  // },
+  // head() {
+  //     return {
+  //         title: 'Sign In - Course Selector'
+  //     }
+  // }
 }
 </script>
+
+<style>
+body,
+html {
+  height: 100%;
+}
+
+.iPhone {
+  height: 300px;
+  width: 300px;
+  /* margin-left: auto;
+  margin-right: auto; */
+  position: relative;
+  float: right;
+  z-index: 1;
+  /* right: 200px; */
+}
+
+.Mac {
+  height: 500px;
+  width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+  position: absolute;
+  right: 100px;
+  z-index: 1;
+}
+
+/* The hero image */
+.hero-image {
+  background-image: url("/icon.png");
+
+  height: 500px;
+  width: 500px;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.LandingPageText {
+  float: left;
+}
+</style>
